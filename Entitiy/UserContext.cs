c883dd79ceab421f3f58d3dd27060eb821ad594e
SrFullStack <rsgithubb@gmail.com>
2023-01-30 20:43:24 +0200

    cach

--- Entitiy/UserContext.cs
+++ Entitiy/UserContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-
+///
 namespace Entitiy
 {
     public partial class UserContext : DbContext
